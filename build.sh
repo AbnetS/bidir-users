1bc9a5c6bd22cf27d5072c5c4d4d973ff19243de
Teferi Assefa <teferi.assefa>
2017-11-06 21:06:44 +0000

    build and docker files have added

--- build.sh
+++ build.sh
@@ -1,26 +1,25 @@
+#!/usr/bin/env bash
 DOCKER_PATH=$(which docker)
-SERVICE_NAME=users-service
+SERVICE_NAME=users-api
 IMAGE_TAG=bidir/$SERVICE_NAME
 EXPOSE_PORT=8030
-
+CONT_PORT=8030
+HOST_IP=10.142.0.2
+MONGODB_URL=mongodb://10.142.0.2:27017/bidir
 # Stop running container
 $DOCKER_PATH stop $SERVICE_NAME
-
 # Remove container
 $DOCKER_PATH rm $SERVICE_NAME
-
 # Remove previous image
 $DOCKER_PATH rmi $IMAGE_TAG
-
 # Build image
 $DOCKER_PATH build -t $IMAGE_TAG .
-
 # Build the container
 $DOCKER_PATH run -d \
   --name $SERVICE_NAME \
-  -p $HOST_IP:$EXPOSE_PORT:$EXPOSE_PORT \
-  -e HOST_IP=$HOST_IP \
-  -e API_URL=$API_URL \
+  -p $HOST:$EXPOSE_PORT:$CONT_PORT \
+  -e HOST=$HOST_IP \
   -e MONGODB_URL=$MONGODB_URL \
   --restart=always \
   $IMAGE_TAG
+
