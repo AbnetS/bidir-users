8029d0cb5cc56584534c332b7b6c583fb0090ee6
Tony Mutai <tonnimutai@gmail.com>
2018-12-13 09:57:34 +0300

    update model

--- models/branch.js
+++ models/branch.js
@@ -12,11 +12,16 @@ var Schema = mongoose.Schema;
 var BranchSchema = new Schema({
     name:           { type: String, required: true, unique: true },
     location:       { type: String, required: true },    
-    opening_date:   { type: Date },
-    branch_type:    { type: String },
-    email:          { type: String },
-    phone:          { type: String },
+    opening_date:   { type: Date, default: null },
+    branch_type:    { type: String, default: '' },
+    email:          { type: String, default: '' },
+    phone:          { type: String, default: '' },
     status:         {type: String, enums:['active', 'inactive'], default:'active'},
+    weredas:        [{ type: Schema.Types.ObjectId, ref: 'Wereda'}],
+    geolocation:    {
+      longitude: { type: Number, default: 0.0 },
+      latitude:  { type: Number, default: 0.0 }
+    },
     date_created:   { type: Date },
     last_modified:  { type: Date }
 });
@@ -48,7 +53,7 @@ BranchSchema.pre('save', function preSaveMiddleware(next) {
  * Filter Branch Attributes to expose
  */
 BranchSchema.statics.attributes = {
-   _id: 1,
+  _id: 1,
   name: 1,
   location: 1,
   opening_date: 1,
@@ -56,6 +61,8 @@ BranchSchema.statics.attributes = {
   email: 1,
   phone: 1,
   status: 1,
+  weredas: 1,
+  geolocation: 1,
   date_created: 1,
   last_modified: 1
 };
